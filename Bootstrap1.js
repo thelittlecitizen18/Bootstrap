6f23a4272d2b1483f3d2fd8ae8d7a20ec6982720
Your Name <thelittlecitizen18@gmail.com>
2020-09-14 15:21:54 +0300

    create json

---
+++ Bootstrap1.js
@@ -0,0 +1,65 @@
+const instrumentData = [
+ {
+    description: "חליל יפה",
+    id: 1,  
+    imagePath: " C:\Code\Bootstrap\halil.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "חליל",
+    price: 50,
+    typeID: 1
+  },
+{
+    description: "מפוחית יפה",
+    id: 2,  
+    imagePath: " C:\Code\Bootstrap\Mapohit.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "מפוחית",
+    price: 35,
+    typeID: 1
+  },
+{
+    description: "חצוצרה ארוכה",
+    id: 3,  
+    imagePath: " C:\Code\Bootstrap\hazozra.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "חצוצרה",
+    price: 1500,
+    typeID: 1
+  },
+{
+    description: "טרומבון קטן",
+    id: 4,  
+    imagePath: " C:\Code\Bootstrap\trombon.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "טרומבון",
+    price: 2400,
+    typeID: 1
+  },
+{
+    description: "פסנתר יפה",
+    id: 5,  
+    imagePath: " C:\Code\Bootstrap\piano.png",
+    instrumentOrders: null,
+    instrumentType: {id: 2, name: "הקשה"},
+    name: "פסנתר",
+    price: 1400,
+    typeID: 2
+  },
+{
+    description: "כינור יפה",
+    id: 6,  
+    imagePath: " C:\Code\Bootstrap\kinor.png",
+    instrumentOrders: null,
+    instrumentType: {id: 3, name: "מיתר"},
+    name: "כינור",
+    price: 2300,
+    typeID: 3
+  },
+];
+
+  
+  
